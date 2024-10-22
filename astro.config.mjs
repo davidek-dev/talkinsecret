88ce4a28d04ababd9ed0f4bc9f8da65026133c3f
david.zeplin <zeplindavid@gmail.com>
2024-10-22 15:37:55 +0200

    project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import netlify from '@astrojs/netlify';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), alpinejs(), mdx()],
+  output: 'server',
+  adapter: netlify()
+});
