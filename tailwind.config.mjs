b2a680b97cb633a612f2b089c30ad72ac6084f19
david.zeplin <zeplindavid@gmail.com>
2024-11-02 03:13:42 +0100

    implemented video feature

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				bandfont: 'Binggo Wood', // Adds a new `font-display` class
+			  }
+		},
 	},
-	plugins: [require('daisyui')],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	
 	daisyui: {
-		themes: ["dark", "cupcake"],
+		themes: [
+		  {
+			myTheme: {
+			  "primary": "#884d45",
+			  "secondary": "#d5c8a4",
+			  "accent": "#d94d4a",
+			  "neutral": "#212c21",
+			  "base-100": "#dae3da",
+			},
+		  },
+		],
 	  },
 }
